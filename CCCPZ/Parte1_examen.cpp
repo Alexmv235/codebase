d1dc585c30e1c9937726d15a41bd3ffe015e0f57
Alexmv235 <a.m.v22305@gmail.com>
2021-06-09 17:19:19 -0600

    exam

--- CCCPZ/Parte1_examen.cpp
+++ CCCPZ/Parte1_examen.cpp
@@ -1,150 +1,180 @@
+//Colegio Científico, PZ
+//Desarrollado por:
+//AlexanderMonteroVargas
+//OscarMarioEspinozaCamacho
+//GeiselThaisHernandezQuesada
+//2021
+
 #include <iostream>
 #include <sstream>
 #include <string.h>
 
 using namespace std;
 
-class Contenedor {
-	private:
-		char hilera[30];
-		int tamano;
-		int cantidad;
-	public:
-		Contenedor(int n) {
-			cantidad = 0;
-			tamano = n;
-			for(int i = 0; i < tamano; i++)
-				hilera[i] = ' ';
+class Contenedor
+{
+private:
+	char hilera[30];
+	int tamano;
+	int cantidad;
+
+public:
+	Contenedor(int n)
+	{
+		cantidad = 0;
+		tamano = n;
+		for (int i = 0; i < tamano; i++)
+			hilera[i] = ' ';
+	}
+	int getCantidad()
+	{
+		return this->cantidad;
+	}
+	int getTamano()
+	{
+		return this->tamano;
+	}
+	void setCantidad(int cantidad)
+	{
+		this->cantidad = cantidad;
+	}
+	void setTamano(int tamano)
+	{
+		this->tamano = tamano;
+	}
+	void insertarElemento(char elem)
+	{
+		if (cantidad < tamano)
+		{
+			hilera[cantidad] = elem;
+			cantidad++;
 		}
-		int getCantidad() {
-			return this->cantidad;
+	}
+	string toString()
+	{
+		stringstream s;
+		for (int i = 0; i < cantidad; i++)
+			s << hilera[i];
+		s << endl;
+		return s.str();
+	}
+
+	void insertarPalabraMinuscula(char letra)
+	{
+		int numAscii = (int)letra;
+		if (numAscii > 64 && numAscii < 91)
+		{
+			numAscii += 32;
 		}
-		int getTamano() {
-			return this->tamano;
-		}
-		void setCantidad(int cantidad) {
-			this->cantidad = cantidad;
-		}
-		void setTamano(int tamano) {
-			this->tamano = tamano;
-		}
-		void insertarElemento(char elem) {
-			if (cantidad < tamano) {
-				hilera[cantidad] = elem;
-				cantidad++;
+		char nuevaLetra = (char)numAscii;
+		insertarElemento(nuevaLetra);
+	}
+
+	void plural()
+	{
+		char ultLetra = hilera[cantidad - 1];
+		if (ultLetra != 's')
+		{
+			if (ultLetra != 'a' && ultLetra != 'e' && ultLetra != 'i' && ultLetra != 'o' && ultLetra != 'u' && ultLetra != 'z')
+			{
+				hilera[cantidad] = 'e';
+				hilera[cantidad + 1] = 's';
+				cantidad += 2;
 			}
-		}
-		string toString() {
-			stringstream s;
-			for (int i = 0; i < cantidad; i++)
-				s << hilera[i];
-			s << endl;
-			return s.str();
-		}
-		
-		void insertarPalabraMinuscula(char letra){
-			int numAscii = (int)letra; 
-			if(numAscii > 64 && numAscii < 91){ 
-				numAscii+=32;    				
-			}
-			char nuevaLetra = (char)numAscii; 
-			insertarElemento(nuevaLetra);
-		}
-			
-		void plural(){
-			char ultLetra = hilera[cantidad-1];
-			if(ultLetra!='s'){
-				if(ultLetra!='a' && ultLetra!='e' && ultLetra!='i' && ultLetra!='o' && ultLetra!='u' && ultLetra!='z'){
-					hilera[cantidad]='e';
-					hilera[cantidad+1]='s';
-					cantidad+=2;
-				}else{
-					if(ultLetra=='z'){
-						hilera[cantidad-1]='c';
-						hilera[cantidad]='e';
-						hilera[cantidad+1]='s';
-						cantidad+=2;
-					}else{
-						hilera[cantidad]='s';
-						cantidad+=1;
-					}
+			else
+			{
+				if (ultLetra == 'z')
+				{
+					hilera[cantidad - 1] = 'c';
+					hilera[cantidad] = 'e';
+					hilera[cantidad + 1] = 's';
+					cantidad += 2;
+				}
+				else
+				{
+					hilera[cantidad] = 's';
+					cantidad += 1;
 				}
 			}
 		}
-			
-		void codifica(){
-			for(int i=0; i<cantidad; i++){
-				switch(hilera[i]){
-					case 'a':
-						hilera[i]='1';
-						break;
-					case 'e':
-						hilera[i]='2';
-						break;
-					case 'i':
-						hilera[i]='3';
-						break;
-					case 'o':
-						hilera[i]='4';
-						break;
-					case 'u':
-						hilera[i]='5';
-						break;
-				}
+	}
+
+	void codifica()
+	{
+		for (int i = 0; i < cantidad; i++)
+		{
+			switch (hilera[i])
+			{
+			case 'a':
+				hilera[i] = '1';
+				break;
+			case 'e':
+				hilera[i] = '2';
+				break;
+			case 'i':
+				hilera[i] = '3';
+				break;
+			case 'o':
+				hilera[i] = '4';
+				break;
+			case 'u':
+				hilera[i] = '5';
+				break;
 			}
 		}
-			
-		bool esPalindroma(){
-			int temp = cantidad-1;
-			for(int i=0; i<cantidad; i++){ //i=0  temp= cant -
-				if(hilera[i]!=hilera[temp]){
-					return false;
-				}
-				temp--;
+	}
+
+	bool esPalindroma()
+	{
+		int temp = cantidad - 1;
+		for (int i = 0; i < cantidad; i++)
+		{ //i=0  temp= cant -
+			if (hilera[i] != hilera[temp])
+			{
+				return false;
 			}
-			return true;
+			temp--;
 		}
+		return true;
+	}
 };
 
+int main(int argc, char *argv[])
+{
 
-int main(int argc, char *argv[]) {
-	
 	Contenedor contenedor(30);
 	string palabra;
 	int n;
 
-	cout<<"********** Comprobador de palabras******"<<endl;
-	cout<<"Palabra: ";
-	cin>> palabra;
-	cout<<"Dijite la cantidad de letras que tiene la palabra"<<endl;
-	cin>>n;
-		for(int i=0; i<n; i++){
+	cout << "********** Comprobador de palabras******" << endl;
+	cout << "Palabra: ";
+	cin >> palabra;
+	cout << "Dijite la cantidad de letras que tiene la palabra" << endl;
+	cin >> n;
+	for (int i = 0; i < n; i++)
+	{
 		contenedor.insertarPalabraMinuscula(palabra[i]);
 	}
-	cout<<"Palabra en minuscula: ";
-	
-	cout<<(contenedor.toString());
-	
-	if(contenedor.esPalindroma()){
-			cout<<"Es palindroma";
-	}else{
-			cout<<"No es palindroma";
+	cout << "Palabra en minuscula: ";
+
+	cout << (contenedor.toString());
+
+	if (contenedor.esPalindroma())
+	{
+		cout << "Es palindroma";
+	}
+	else
+	{
+		cout << "No es palindroma";
 	}
 	contenedor.plural();
-	cout<<endl<<"Palabra en plural: ";
-	cout<<(contenedor.toString());
-	
+	cout << endl
+		 << "Palabra en plural: ";
+	cout << (contenedor.toString());
+
 	contenedor.codifica();
-	cout<<"Palabra codificada: ";
-	cout<<(contenedor.toString());	
-	cout<<endl;
+	cout << "Palabra codificada: ";
+	cout << (contenedor.toString());
+	cout << endl;
 	return 0;
 }
-
-
-
-
-
-
-
-
--- CCCPZ/Parte2_examen.cpp
+++ CCCPZ/Parte2_examen.cpp
@@ -2,7 +2,7 @@
 //Desarrollado por:
 //AlexanderMonteroVargas
 //OscarMarioEspinozaCamacho
-//GeiselThaisHernandez
+//GeiselThaisHernandezQuesada
 //2021
 
 #include <iostream>
@@ -13,177 +13,204 @@ using namespace std;
 
 const int tam = 7;
 
-class ContenedorMatriz {
-	private:
-		int ventaProd[tam][tam];
-	public:
-		ContenedorMatriz(){
-			for(int i = 0; i<tam; i++){
-				for(int j = 0; j<tam; j++){
-					ventaProd[i][j]=0;
-				}
-			}
-		}
-		// Destructor
-		~ContenedorMatriz() {
-			
-		//Set&get
-		}
-		void setPos (int i, int j, int valor) {
-			ventaProd[i][j]=valor;
-		}
-		int getPos (int i, int j) {
-			return ventaProd[i][j];
-		}
+class ContenedorMatriz
+{
+private:
+	int ventaProd[tam][tam];
 
-		//Llenar matriz
-		void llenarMatriz() {
-			int num;
-			srand(time(NULL));
-			for (int i = 0; i < tam; i++)
-				for (int j = 0; j < tam; j++) {
-					num = 1 + rand() % (100 - 1);
-					ventaProd[i][j]=num;
-			}
-		}
-
-		// Obtener suma del producto toda la semana
-		int getProductoSemana(int producto){
-			int sumaProducto = 0;
-			for(int j=0; j<7; j++){
-				sumaProducto+= ventaProd[producto][j];
-			}
-			return sumaProducto;
-		}
-		// Metodo para seber si se vendió toda la semana	
-		int seVendioProductoSemana(int producto){
-			for(int j=0; j<7; j++){
-				if(ventaProd[producto][j]==0){
-					return false;
-				}
-			}
-			return true;
-		}
-		// Para saber el dia con mas ventas
-		int diaMayorVentasSemana(int producto){
-			int mayorVentaDia = 0;
-			int mayorVentaValor = ventaProd[producto][0];
-			for(int j=1; j<7; j++){
-				if(ventaProd[producto][j]>mayorVentaValor){
-					mayorVentaValor = ventaProd[producto][j];
-					mayorVentaDia = j;
-				}
-			}
-			return mayorVentaDia;
-		}
-		//Para	saber el producto menos vendido en el dia
-		int productoMenosVentasDia(int dia){
-			int menorVentaProducto = 0;
-			int menorVentaValor = ventaProd[0][dia];
-			for(int i=1; i<7; i++){
-				if(ventaProd[i][dia]<menorVentaValor){
-					menorVentaValor = ventaProd[i][dia];
-					menorVentaProducto = i;
-				}
-			}
-			return menorVentaProducto;
-		}
-		//string to string
-		string toString() {
-		stringstream s;
-		for (int i = 0; i < tam; i++) {
+public:
+	ContenedorMatriz()
+	{
+		for (int i = 0; i < tam; i++)
+		{
 			for (int j = 0; j < tam; j++)
-				s << ventaProd[i][j]<< "\t";
+			{
+				ventaProd[i][j] = 0;
+			}
+		}
+	}
+	// Destructor
+	~ContenedorMatriz()
+	{
+
+		//Set&get
+	}
+	void setPos(int i, int j, int valor)
+	{
+		ventaProd[i][j] = valor;
+	}
+	int getPos(int i, int j)
+	{
+		return ventaProd[i][j];
+	}
+
+	//Llenar matriz
+	void llenarMatriz()
+	{
+		int num;
+		srand(time(NULL));
+		for (int i = 0; i < tam; i++)
+			for (int j = 0; j < tam; j++)
+			{
+				num = 1 + rand() % (100 - 1);
+				ventaProd[i][j] = num;
+			}
+	}
+
+	// Obtener suma del producto toda la semana
+	int getProductoSemana(int producto)
+	{
+		int sumaProducto = 0;
+		for (int j = 0; j < 7; j++)
+		{
+			sumaProducto += ventaProd[producto][j];
+		}
+		return sumaProducto;
+	}
+	// Metodo para seber si se vendió toda la semana
+	int seVendioProductoSemana(int producto)
+	{
+		for (int j = 0; j < 7; j++)
+		{
+			if (ventaProd[producto][j] == 0)
+			{
+				return false;
+			}
+		}
+		return true;
+	}
+	// Para saber el dia con mas ventas
+	int diaMayorVentasSemana(int producto)
+	{
+		int mayorVentaDia = 0;
+		int mayorVentaValor = ventaProd[producto][0];
+		for (int j = 1; j < 7; j++)
+		{
+			if (ventaProd[producto][j] > mayorVentaValor)
+			{
+				mayorVentaValor = ventaProd[producto][j];
+				mayorVentaDia = j;
+			}
+		}
+		return mayorVentaDia;
+	}
+	//Para	saber el producto menos vendido en el dia
+	int productoMenosVentasDia(int dia)
+	{
+		int menorVentaProducto = 0;
+		int menorVentaValor = ventaProd[0][dia];
+		for (int i = 1; i < 7; i++)
+		{
+			if (ventaProd[i][dia] < menorVentaValor)
+			{
+				menorVentaValor = ventaProd[i][dia];
+				menorVentaProducto = i;
+			}
+		}
+		return menorVentaProducto;
+	}
+	//string to string
+	string toString()
+	{
+		stringstream s;
+		for (int i = 0; i < tam; i++)
+		{
+			for (int j = 0; j < tam; j++)
+				s << ventaProd[i][j] << "\t";
 			s << endl;
 		}
 		s << endl;
 		return s.str();
-	}	
+	}
 };
 
+int main(int argc, char *argv[])
+{
 
-
-int main(int argc, char *argv[]) {
-	
 	//Para mostrar los productos y los dias en el resultado.
 	string productos[] = {"Arroz", "Frijoles", "Azucar", "Leche", "Harina", "Huevos", "Agua"};
 	string dias[] = {"Lunes", "Martes", "Miercoles", "Jueves", "Viernes", "Sabado", "Domingo"};
-	
+
 	//Mostrar lista de productos y dias
-	cout<<"Lista de Productos:"<<endl;
-	cout<<"Arroz - 0"<<endl;
-	cout<<"Frijoles - 1"<<endl;
-	cout<<"Azucar - 2"<<endl;
-	cout<<"Leche - 3"<<endl;
-	cout<<"Harina - 4"<<endl;
-	cout<<"Huevos - 5"<<endl;
-	cout<<"Agua - 6"<<endl;
-	cout<<endl<<endl;
-	cout<<"Lista de Dias:"<<endl;
-	cout<<"Lunes - 0"<<endl;
-	cout<<"Martes - 1"<<endl;
-	cout<<"Miercoles - 2"<<endl;
-	cout<<"Jueves - 3"<<endl;
-	cout<<"Viernes - 4"<<endl;
-	cout<<"Sabado - 5"<<endl;
-	cout<<"Domingo - 6"<<endl;
-	cout<<endl<<endl;
-	
+	cout << "Lista de Productos:" << endl;
+	cout << "Arroz - 0" << endl;
+	cout << "Frijoles - 1" << endl;
+	cout << "Azucar - 2" << endl;
+	cout << "Leche - 3" << endl;
+	cout << "Harina - 4" << endl;
+	cout << "Huevos - 5" << endl;
+	cout << "Agua - 6" << endl;
+	cout << endl
+		 << endl;
+	cout << "Lista de Dias:" << endl;
+	cout << "Lunes - 0" << endl;
+	cout << "Martes - 1" << endl;
+	cout << "Miercoles - 2" << endl;
+	cout << "Jueves - 3" << endl;
+	cout << "Viernes - 4" << endl;
+	cout << "Sabado - 5" << endl;
+	cout << "Domingo - 6" << endl;
+	cout << endl
+		 << endl;
+
 	// Llenar e imprimir, deja un espacio con valor 0 para probar el metodo de "seVendioProductoSemana"
 	ContenedorMatriz contenedor1;
 	contenedor1.llenarMatriz();
 	contenedor1.setPos(3, 4, 0);
-	cout<<contenedor1.toString();
-	cout<<endl<<endl;
-	
+	cout << contenedor1.toString();
+	cout << endl
+		 << endl;
+
 	//Solicitar producto y dia para definir cantidad.
 	int producto, dia;
-	cout<<"Digite el numero del producto mostrado en la lista: ";
-	cin>>producto;
-	cout<<endl<<"Digite el numero del dia mostrado en la lista: ";
-	cin>>dia;
-	cout<<endl<<"El dia "<<dias[dia]<<" se vendio la cantidad de "<<contenedor1.getPos(producto, dia)<<" del producto "<<productos[producto];
-	cout<<endl;
-	
+	cout << "Digite el numero del producto mostrado en la lista: ";
+	cin >> producto;
+	cout << endl
+		 << "Digite el numero del dia mostrado en la lista: ";
+	cin >> dia;
+	cout << endl
+		 << "El dia " << dias[dia] << " se vendio la cantidad de " << contenedor1.getPos(producto, dia) << " del producto " << productos[producto];
+	cout << endl;
+
 	// Digita el producto para saber la cantidad de veces que se vendio en la semana
-	cout<<endl;
-	cout<<"Digite el numero del producto para saber cuantas veces se vendio en la semana: ";
-	cin>>producto;
-	cout<<endl<<"Durante la semana se vendio la cantidad de "<<contenedor1.getProductoSemana(producto)<<" del producto "<<productos[producto];
-	cout<<endl;
-	
+	cout << endl;
+	cout << "Digite el numero del producto para saber cuantas veces se vendio en la semana: ";
+	cin >> producto;
+	cout << endl
+		 << "Durante la semana se vendio la cantidad de " << contenedor1.getProductoSemana(producto) << " del producto " << productos[producto];
+	cout << endl;
+
 	// Digita el producto para saber si se vendio toda la semana o no
-	cout<<endl;
-	cout<<"Digite el numero del producto para saber si se vendio todos los dias de la semana: ";
-	cin>>producto;
-	if(contenedor1.seVendioProductoSemana(producto)){
-		cout<<"El producto "<<productos[producto]<<" se vendio durante toda la semana";
-	}else{
-		cout<<"El producto "<<productos[producto]<<" no se vendio algun dia durante toda la semana";
+	cout << endl;
+	cout << "Digite el numero del producto para saber si se vendio todos los dias de la semana: ";
+	cin >> producto;
+	if (contenedor1.seVendioProductoSemana(producto))
+	{
+		cout << "El producto " << productos[producto] << " se vendio durante toda la semana";
 	}
-	cout<<endl<<endl;
-	
+	else
+	{
+		cout << "El producto " << productos[producto] << " no se vendio algun dia durante toda la semana";
+	}
+	cout << endl
+		 << endl;
+
 	//Digita el prodcuto para saber cual fue el día de mayor venta
-	cout<<endl;
-	cout<<"Digite el numero del producto para saber cual fue el dia de mayor venta: ";
-	cin>>producto;
-	cout<<endl<<"El dia "<<dias[contenedor1.diaMayorVentasSemana(producto)]<<" se hicieron mas ventas del producto "<<productos[producto];
-	cout<<endl<<endl;
+	cout << endl;
+	cout << "Digite el numero del producto para saber cual fue el dia de mayor venta: ";
+	cin >> producto;
+	cout << endl
+		 << "El dia " << dias[contenedor1.diaMayorVentasSemana(producto)] << " se hicieron mas ventas del producto " << productos[producto];
+	cout << endl
+		 << endl;
 
 	//Digita el dia para saber cual fue el producto menos vendido
-	cout<<endl;
-	cout<<"Digite el numero del dia para saber cual fue el producto menos vendido: ";
-	cin>>dia;
-	cout<<endl<<"El dia "<<dias[dia]<<" se hicieron menos ventas del producto "<<productos[contenedor1.productoMenosVentasDia(dia)];
-	cout<<endl;
-	
+	cout << endl;
+	cout << "Digite el numero del dia para saber cual fue el producto menos vendido: ";
+	cin >> dia;
+	cout << endl
+		 << "El dia " << dias[dia] << " se hicieron menos ventas del producto " << productos[contenedor1.productoMenosVentasDia(dia)];
+	cout << endl;
+
 	return 0;
 }
-
-
-
-
-
-
-
-
