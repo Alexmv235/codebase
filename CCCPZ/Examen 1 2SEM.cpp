d403dc92f7ba54dcd33b86fbd3108121b240c701
Alexmv235 <a.m.v22305@gmail.com>
2021-09-04 22:38:58 -0600

    code

--- CCCPZ/Examen 1 2SEM.cpp
+++ CCCPZ/Examen 1 2SEM.cpp
@@ -5,6 +5,7 @@
 #include <iostream>
 #include <windows.h> 
 #include <conio.h>
+#include<time.h>
 using namespace std;
 class clsColeccion
 {
@@ -42,12 +43,24 @@ public:
 	{
 		for(int i=0;i<4; i++)
 		{
-			Flores[0]="Hortensias";
-			Flores[1]="Rosas";
-			Flores[2]="Tulipanes";
-			Flores[3]="Helechos";
+			Flores[0]="hortensias";
+			Flores[1]="rosas";
+			Flores[2]="tulipanes";
+			Flores[3]="helechos";
 		}
 	}
+	
+	void definirPrecios()
+	{
+		for(int i=0;i<4; i++)
+		{
+			Precios[0]=2000;
+			Precios[1]=1000;
+			Precios[2]=3000;
+			Precios[3]=500;
+		}
+	}
+	
 	void definirSurcos()
 	{
 		for(int i=0;i<9; i++)
@@ -136,6 +149,7 @@ public:
 	//Plantas crecen
 	void plantasCrecen()
 	{
+		srand(time(NULL));
 		for (int i=0; i<9; i++)
 		{
 			for(int j=0; j<4; j++)
@@ -145,7 +159,107 @@ public:
 		}
 	}
 	
+    //Metodo para saber cual planta
+	void conocerCosecha()
+	{
+		string nPlanta = " ";
+		int plantaNum =0;
+		int precioPlant =0;
+		cout<<endl<<"dijite el nombre de la planta (escriba todo en minusculas)"<<endl;
+		cin>>nPlanta;
+		
+		if (nPlanta == "hortensias" )
+		{
+			plantaNum = 0;
+		}
+		else if (nPlanta == "rosas")
+		{
+			plantaNum = 1;
+		}
+		else if (nPlanta == "tulipanes")
+		{
+			plantaNum = 2;
+		}
+		else if (nPlanta == "helechos")
+		{
+			plantaNum = 3;
+		}
+		int cant=-1;
+		int cantM=0;
+			for(int i=0; i<9; i++)
+			{
+				if(Terreno[i][plantaNum]>2)
+				{
+					cant++;
+				}
+				if (cant>cantM)
+				{
+					cantM=cant;
+					precioPlant=(Precios[plantaNum] * cantM);
+				}
+				
+			}
+			cout<<endl<<"La planta: "<<Flores[plantaNum]<< " tiene: "<<cantM<< " plantas disponibles para cosechar"<<endl;
+			cout<<endl<< "La ganancia de vender todas la plantas listas de: "<<Flores[plantaNum]<<" es:" <<precioPlant<<endl;
+	}
+	// conocer plantas listas en todo el Terreno
+	void conocerCosechaTerreno()
+	{
+		int cant=0;
+		int cantM=0;
+		for (int i=0; i<9; i++)
+		{
+			for(int j=0; j<4; j++)
+			{	
+			if(Terreno[i][j]>2)
+			{
+				cant++;
+			}
+			if (cant>cantM)
+			{
+				cantM=cant;
+			}
+			}
+		}
+		cout<<endl<<"En total hay: "<<cantM<< " plantas disponibles para cosechar"<<endl;
+	}
 	
+	//Precios
+	void mostrarPrecios()
+	{
+		cout<<"Los precios son"<<endl;
+		cout<<endl;
+		for (int i=0;i<4; i++) 
+		{
+			cout<<"\t"<<Precios[i]<<endl;
+		}
+		cout<<endl;
+	}
+	
+	// Monto total Fila
+	void montoTotalFila()
+	{
+		int cantH,cantR,cantT,cantE=0;
+		int cantTH, cantTR,cantTT,cantTE=0;
+		int precioH, precioR, precioT, precioE =0;
+		int surco, numSurco =0;
+		cout<<"escriba el número de surco que quiere consultar"<<endl;
+		cin>>surco;
+		numSurco= (surco - 1);
+			for(int j=0; j<4; j++)
+			{	
+				if(Terreno[numSurco][j]>2)
+				{
+					cantH++;
+				}
+				if (cantH>cantTH)
+				{
+					cantTH=cantH;
+					precioH=(Precios[plantaNum] * cantM);
+				}
+			}
+		cout<<endl<<"En total hay: "<<cantH<< " plantas disponibles para cosechar"<<endl;
+	}
 };
 int main(int argc, char *argv[]) {
 	cout<<"FARM MANAGER 2021"<<endl; 
@@ -164,6 +278,13 @@ int main(int argc, char *argv[]) {
 	Obj.mostrarTerreno();
 	Obj.plantasCrecen();
 	Obj.mostrarTerreno();
+	Obj.definirPrecios();
+	Obj.mostrarPrecios();
+	Obj.conocerCosecha();
+	Obj.conocerCosechaTerreno();
+	
+	
+	
 	
 	return 0;
 }
