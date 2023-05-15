3dfc1a0ec56b6ab0f0ab83ebe0414901cdeb1447
Alexmv235 <a.m.v22305@gmail.com>
2023-05-14 22:39:43 -0600

    binarioscon,decimales

--- TEC/ITERACION/Tarea7/cnv_binario.py
+++ TEC/ITERACION/Tarea7/cnv_binario.py
@@ -1,15 +1,42 @@
 def convertir_binario(num):
-    if isinstance(num,(int,float)):
-        result=0
-        i=0
+    result=0
+    i=0
+    j=1
+    k=1
+    if isinstance(num,int):
         while num!=0:
-            result+=num%10*(2**i)
-            num//=10
-            i+=1
+            if num%10 ==1 or num%10 ==0:
+                result+=num%10*(2**i)
+                num//=10
+                i+=1
+            else:
+                return "error, no es binario"
         return result
+    elif isinstance(num,float):
+        entero=num//1
+        decimales=num-entero/1
+        while entero!=0:
+            if entero%10 ==1 or entero%10 ==0:
+                result+=entero%10*(2**i)
+                entero//=10
+                i+=1
+            else:
+                return "error, no es binario"
+        while decimales!=0 and k<4:
+            decimales*=10
+            dig=(decimales%10)//1
+            if dig ==1 or dig ==0:
+                result+=(dig/(2**j))
+                decimales-dig
+                j+=1
+                k+=1
+            else:
+                return "error, hay un decimal no binario"
+        return result
+        
     else:
         return "error"
     
 print(
-    convertir_binario(0)
+    convertir_binario(1010.10101)
 )
