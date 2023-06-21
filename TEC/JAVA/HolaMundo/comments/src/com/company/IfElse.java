e2668bf7e0906609d2f7fbcc984b8f4608949971
Alexmv235 <a.m.v22305@gmail.com>
2023-06-20 21:39:21 -0600

    ifelse

---
+++ TEC/JAVA/HolaMundo/comments/src/com/company/IfElse.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class IfElse {
+    public static void main(String[] args) {
+        
+        boolean check = 5<10;
+
+        if (check) {
+            System.out.println(true);
+        } else {
+            System.out.println(false);
+        }
+
+        
+    }
+}
